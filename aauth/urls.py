de6109c07175f11cb875ff2c34626f458f03b385
Oralbek Aiod <or.aidos06@gmail.com>
2025-04-26 01:33:28 +0500

    Fix merge conflicts and update views.py

--- aauth/urls.py
+++ aauth/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # aauth/urls.py
 from django.urls import path
 from . import views
@@ -10,17 +9,3 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
-
-=======
-from django.urls import path
-from . import views
-
-app_name = 'aauth'
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout'),
-]
-
->>>>>>> 5c41cd8 (Changes with API)

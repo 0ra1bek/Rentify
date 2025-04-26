5d60492e114141e401417aacd40d3343fcb8934e
nursz <nurboysuper6123@gmail.com>
2025-04-26 10:44:29 +0500

    Changed setting.py

--- Rentify/settings.py
+++ Rentify/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'rentify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rentify_db_Mysql',  
+        'USER': 'root',  
+        'PASSWORD': 'RenAidNur#2025', 
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation

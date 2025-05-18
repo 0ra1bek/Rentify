49d67b065ad1b803cfb1f735938595d56f520fad
nursz <nurboysuper6123@gmail.com>
2025-05-18 20:18:25 +0500

    Added some details

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile
+
+# Класс для настройки отображения модели UserProfile в админке
+class UserProfileAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться в списке записей
+    list_display = ('username', 'email', 'age', 'date_joined')
+    
+    # Поля, по которым можно производить поиск
+    search_fields = ('username', 'email')
+    
+    # Поля, по которым можно фильтровать записи
+    list_filter = ('date_joined', 'age')
+    
+    # Поля, которые можно редактировать прямо из списка
+    list_editable = ('age', 'email')
+    
+    # Поля, которые будут использоваться для автоматического заполнения (если нужно)
+    prepopulated_fields = {}
+
+# Регистрация модели UserProfile с настройками UserProfileAdmin
+admin.site.register(UserProfile, UserProfileAdmin)
--- main1/views.py
+++ main1/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Property
 
 def index(request):
-    return render(request ,'main1/main1.html')
+    rooms_filter = request.GET.get('rooms')
+    properties = Property.objects.all()
+    if rooms_filter:
+        properties = properties.filter(rooms=rooms_filter)
+    return render(request, 'main1/main1.html', {'properties': properties})
